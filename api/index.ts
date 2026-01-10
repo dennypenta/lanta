0680484cd3cfc02095c6004012da6aa30b54b36f
Denis Dvornikov <candyboobers@gmail.com>
2026-01-10 11:59:39 +0100

    move back to bun

--- api/index.ts
+++ api/index.ts
@@ -1,35 +1,27 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-
 import newConfig from './config.ts'
 
-const conf = newConfig()
+const config = newConfig()
 
-const app = new Hono()
-
-app.get('/health', (c) => {
-  c.status(200)
-  return c.text('')
+const server = Bun.serve({
+  port: config.port,
+  routes: {
+    '/health': new Response(null, { status: 200 }),
+  },
 })
 
-const server = serve({
-  fetch: app.fetch,
-  port: conf.port,
-})
-
-console.debug(`server started on port=${conf.port}`)
+console.log(`Server running at ${server.url}`)
 
 // graceful shutdown
-process.on('SIGINT', () => {
-  server.close()
-  process.exit(0)
-})
-process.on('SIGTERM', () => {
-  server.close((err) => {
-    if (err) {
-      console.error(err)
-      process.exit(1)
-    }
+const handleShutdown = () => {
+  setTimeout(() => {
+    console.warn('shutdown timeout, force exit')
+    process.exit(1)
+  }, 10 * 1000)
+
+  console.log('server stopping')
+  server.stop().then(() => {
     process.exit(0)
   })
-})
+}
+process.on('SIGINT', handleShutdown)
+process.on('SIGTERM', handleShutdown)
